c8322e446146ce7b6590e345443ab1efc6d071af
amayl4 <amayl4@outlook.com>
2024-05-10 23:27:43 +0100

    linear search in python

---
+++ linear-search.py
@@ -0,0 +1,10 @@
+arr = [12, 54, 76, 56, 34, 75, 98, 103, 451]
+
+# return the index of any number in the array using linear search
+
+target = int(input("Enter target number: \n")) # allowing user to select the target
+
+for num in range(0, len(arr)):
+    if arr[num] == target:
+        print(num+1) # array indexing always gives the (n-1)th index so +1 returns the nth index
+        break # stops the for loop from going on forever
