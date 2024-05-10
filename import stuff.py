fa9828753451ed8fdfa35996686a3cea11a81827
amayl4 <amayl4@outlook.com>
2024-05-10 23:20:06 +0100

    basic quadratic equation solver for ax^2 + bx + c = 0

--- import stuff.py
+++ import stuff.py
@@ -1,2 +1,15 @@
-import numpy as np
-from scipy.integrate import solve_ivp
+# basic quadratic equation solution
+
+def solveQE(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant < 0:
+        print("Equation has no real roots")
+    elif discriminant == 0:
+        x = (-1*b) / (2*a)
+        print("Repeated root at x = ", x)
+    elif discriminant > 0:
+        x1 = (1*b + discriminant**0.5) / 2*a
+        x2 = (1*b - discriminant**0.5) / 2*a
+        print("Roots are ", x1, " and ", x2)
+
+solveQE(1, 4, 4)
