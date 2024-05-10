16db748fb9271130e3cbf1aef5792757fa24b4d0
amayl4 <amayl4@outlook.com>
2024-05-10 23:30:23 +0100

    python bubble sort

---
+++ bubble-sort.py
@@ -0,0 +1,11 @@
+# Sort numbers in ascending order w/ bubble sort
+
+arr = [12, 54, 76, 56, 34, 75, 98, 103, 451]
+n = len(arr)
+
+for i in range(n):
+    for j in range(n-1):
+        if arr[i] < arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
+
+print("Sorted list is:\n", arr)
