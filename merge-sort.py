c81c2b74d9db2ee2e0e67e1b5815d4196582bf73
amayl4 <amayl4@outlook.com>
2024-05-11 10:23:47 +0100

    merge sort

---
+++ merge-sort.py
@@ -0,0 +1,35 @@
+array = [12, 54, 76, 56, 34, 75, 98, 103, 451]
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        # recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # merge
+        i = 0
+        j = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+merge_sort(array)
+print(array)
