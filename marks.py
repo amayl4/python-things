07bff045d0b1661ce0ac771c5c7c06f66f39ba1a
amayl4 <amayl4@outlook.com>
2024-05-22 11:45:28 +0100

    created new file

---
+++ marks.py
@@ -0,0 +1,12 @@
+'''
+The marks obtained by a student in 8 different subjects are input through the
+keyboard. Write a program to find the total marks and percentage marks
+obtained by each student. Assume that the maximum marks that can be
+obtained by a student in each subject is 100.
+'''
+
+# 1. input the subject marks
+# 2. find the total of the marks
+# 3. return the total marks out of 800
+# 4. find the percentage by dividing by 800 then multiplying by 100
+
