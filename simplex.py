95365d9f78e619282261d1a5576a4d9a6576dfa3
amayl4 <amayl4@outlook.com>
2024-05-22 12:41:39 +0100

    finished simplex algo

--- simplex.py
+++ simplex.py
@@ -1,7 +1,25 @@
-import numpy as np
+from scipy.optimize import linprog
 
-# Maximise P = 3x + 4y + 10z
-'''
-Subject to:
-x
-'''
+# Coefficients of the objective function (negated for maximization)
+c = [-2, -3]
+
+# Coefficients of the inequality constraints (left-hand side)
+A = [
+    [1, 1],
+    [2, 1]
+]
+
+# Right-hand side of the inequality constraints
+b = [4, 5]
+
+# Bounds for the variables (x >= 0, y >= 0)
+x_bounds = (0, None)
+y_bounds = (0, None)
+
+# Solve the problem
+result = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds], method='simplex')
+
+# Print the results
+print('Optimal value:', -result.fun)
+print('x:', result.x[0])
+print('y:', result.x[1])
