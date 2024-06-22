8e4c8ae4bad9423b28fa2146969906f70dccc374
amayl4 <165853721+amayl4@users.noreply.github.com>
2024-06-22 19:12:45 +0100

    Update bubble-sort.py

--- misc/poopy/bubble-sort.py
+++ misc/poopy/bubble-sort.py
@@ -4,7 +4,7 @@ arr = [12, 54, 76, 56, 34, 75, 98, 103, 451]
 n = len(arr)
 
 for i in range(n): # loop through the array once
-    for j in range(n-1): # loop through the array again
+    for j in range(n-i-1): # loop through the array again
         if arr[i] < arr[j]: # compare the 2 values
             arr[i], arr[j] = arr[j], arr[i] # make the actual swaps
 
